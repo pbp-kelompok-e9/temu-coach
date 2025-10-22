3ddd79c4df4aacd77fcd235d3275f8fb05af3e45
ericmaeda <ericmaeda41@gmail.com>
2025-10-22 19:14:45 +0700

    fix: updated some functions on views.py

--- reviews_ratings/views.py
+++ reviews_ratings/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
 from .models import Reviews
+from coaches_book_catalog.models import Coach
 
 # Create your views here.
+@require_POST
 def create_review(request, coach_id):
-    if request.method == "POST" :
-        Reviews.objects.create(
-            coach_id = request.POST['coach_id'],
-            user = request.user,
-            rate = request.POST['rate'],
-            review = request.POST['review']
-        )
-        return JsonResponse({"success": True})
+    rate = request.POST.get('rate')
+    review = request.POST.get('review', '')
+    coach = get_object_or_404(Coach, id=coach_id)
     
+    Reviews.objects.update_or_create(
+        coach = coach,
+        user = request.user,
+        defaults = {
+            'rate': rate,
+            'review': review,
+        }
+    )
+    return JsonResponse({"success": True})
+
+@require_POST
 def update_review(request, id) :
     review = Reviews.objects.get(id=id, user=request.user)
-    review.rate = request.POST['rate']
-    review.review = request.POST['review']
+    review.rate = request.POST.get('rate')
+    review.review = request.POST.get('review')
     review.save()
     return JsonResponse({"success": True})
 
+@require_POST
 def delete_review(request, id) :
     review = Reviews.objects.get(id=id, user=request.user)
     review.delete()
     return JsonResponse({"success": True})
+
 
 def check_review(request, coach_id) :
     try :
