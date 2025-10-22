07419b8cadf1b4a4397e8e618aab42c60ed400b2
BenedictZir <benedictusziraluo@gmail.com>
2025-10-23 01:10:56 +0700

    feat: navbar

--- temu_coach/settings.py
+++ temu_coach/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-    
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ykg!sglrgw7k_q-^qyf5**i34qm)-l944+tac!c(ji%phs8x&8'
 
@@ -144,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
