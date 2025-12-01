e46e9fe2e1ea0ed468e05c7316bfb184e5722f24
Alvino0706 <alvino0706@gmail.com>
2025-12-01 14:01:32 +0700

    Edit urls.py on admin

--- my_admin/urls.py
+++ my_admin/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('coach/<int:coach_id>/ban/', views.ban_coach, name='ban_coach'),
     path('report/<int:report_id>/delete/', views.delete_report, name='delete_report'),
 
-    path('api/reports/', views.admin_reports_api, name='admin_reports_api'),
-    path('api/coach-requests/', views.admin_coach_requests_api, name='admin_coach_requests_api'),
     path('api/coach/<int:coach_id>/approve/', views.approve_coach_api, name='approve_coach_api'),
     path('api/coach/<int:coach_id>/reject/', views.reject_coach_api, name='reject_coach_api'),
     path('api/coach/<int:coach_id>/ban/', views.ban_coach_api, name='ban_coach_api'),
